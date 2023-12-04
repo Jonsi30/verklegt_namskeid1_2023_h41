ea22fa02f6e1154c2a0cf55b49d11fdb82e477bb
Jón Ágústsson <jonsarinn30@gmail.com>
2023-12-04 14:52:40 +0000

    voyage_data

--- Data_layer/voyaga_data.py
+++ Data_layer/voyaga_data.py
@@ -1,9 +1,12 @@
 import csv
 
 class Voyage_data:
-    def __init__(self): 
-        self.file_name = "files/voyage.csv" #filenameið sem inniheldur info um þetta class
-        pass
+    def __init__(self):
+        try: 
+            self.file_name = input(f"Enter voyage filename: ") #filenameið sem inniheldur info um þetta class
+            self.file = open(self.file_name, "r")             
+        except FileNotFoundError:
+            print("File not found, try again")
 
     def print_crew_list_for_voyage(self):
         pass
@@ -16,5 +19,9 @@ class Voyage_data:
         pass
 
     def read_voyage_information(self):
+        flight_dict = dict()
         """User can read information about a voyage"""
-        pass
+        for line in self.file:
+            flight_nr, dep_from, arr_at, departure, arrival, plane_insignia = line
+            flight_dict[flight_nr] = dep_from, arr_at, departure, arrival, plane_insignia
+        
