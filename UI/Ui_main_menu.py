73a26d213d4859a0bdd55fc032e09f5ba7e41869
oppenheimm <tommibragi03@gmail.com>
2023-12-04 14:14:42 +0000

    manager and director screens

--- UI/Ui_main_menu.py
+++ UI/Ui_main_menu.py
@@ -40,17 +40,50 @@ class Main_menu:
 
                                 or [q] quit
                     """
-        
+
         print(landing_ui)
         """Returns the input from the user"""
-        input_str = input("login").lower()
+        input_str = input("login: ").lower()
         return input_str
-    
+
     def manager_menu():
         """Prints the manager menu for the user"""
+        clear_screen()
         manager_ui = """
+    Welcome Manager 
+
+    [0] Voyage List  
+    [1] View All Employees
+
+    [q] Quit 
+    [b] Back
+    """
+        print(manager_ui)
+        input_str = input("Select Option: ").lower()
+        return input_str
+
+    def director_menu():
+        """prints director page and asks for input"""
+
+        clear_screen()
+        """Prints the director menu for the user"""
+        director_ui = """
+    Welcome Director 
+
+    [0] Voyage List  
+    [1] Create Voyage
+    [2] View All Airplanes
+    [3] View All Destinations
 
 
-    def clear_screen(self):
+    [q] Quit 
+    [b] Back
+    """
+
+        print(director_ui)
+        input_str = input("Select Option: ").lower()
+        return input_str
+
+    def clear_screen():
         """Clears the screen"""
         os.system("cls" if os.name == "nt" else "clear")
