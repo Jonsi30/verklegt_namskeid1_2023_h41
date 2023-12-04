e28092c279716eee58eb913c2f6b1b6b92fdc9c7
Jón Ágústsson <jonsarinn30@gmail.com>
2023-12-04 14:01:09 +0000

    my changes

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
-from UI.ui_main_menu import Main_menu
-
+from UI.Ui_main_menu import Main_menu
 
 def main():
-    Main_menu.landing_page
-
+    user_input = Main_menu.landing_page()
+    if user_input == "m":
+        Main_menu.manager_menu()
+    elif user_input == "d":
+        Main_menu.director_menu()
+    print("help")
+    print("Hello world")
 
 if __name__ == "__main__":
     main()
