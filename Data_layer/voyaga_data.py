c9fac83f1ba29b3f8374a0fb9a00a6858ff3d6a3
steinarorn <steinar98@gmail.com>
2023-12-04 14:42:19 +0000

    data

--- Data_layer/voyaga_data.py
+++ Data_layer/voyaga_data.py
@@ -5,16 +5,23 @@ class Voyage_data:
         self.file_name = "files/voyage.csv" #filenameið sem inniheldur info um þetta class
         pass
 
+
+
+
+
+"""
     def print_crew_list_for_voyage(self):
         pass
 
     def create_voyage(self):
-        """User can Create Voyage"""
+        #User can Create Voyage#
         pass
 
     def edit_voyage(self):
         pass
 
     def read_voyage_information(self):
-        """User can read information about a voyage"""
+        #User can read information about a voyage#
         pass
+        
+"""
