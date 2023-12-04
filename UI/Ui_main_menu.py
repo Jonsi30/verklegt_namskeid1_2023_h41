b7a5d7595b34443b1ec09fdf0ce27a60b1400c49
oppenheimm <tommibragi03@gmail.com>
2023-12-04 13:01:33 +0000

    main menu

---
+++ UI/Ui_main_menu.py
@@ -0,0 +1,12 @@
+import os
+
+
+class Main_menu:
+    def __init__(self):
+        pass
+
+    def landing_page(self):
+        landing_ui = """"""
+
+    def clear_screen(self):
+        os.system("cls" if os.name == "nt" else "clear")
