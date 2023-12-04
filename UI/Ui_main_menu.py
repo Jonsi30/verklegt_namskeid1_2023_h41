6c975a326e9723c94fd77b34e4cd3f876bae993c
oppenheimm <tommibragi03@gmail.com>
2023-12-04 13:16:53 +0000

    loading screem

--- UI/Ui_main_menu.py
+++ UI/Ui_main_menu.py
@@ -3,9 +3,11 @@ import os
 
 class Main_menu:
     def __init__(self):
+        """Constructor for Main_menu"""
         pass
 
     def landing_page(self):
+        """Prints the landing page for the user"""
         landing_ui = """
 
 
@@ -26,11 +28,11 @@ class Main_menu:
                                    //||\\
                                    '/||\'                                                                     
                                                                       
-    /|    / /                /|    / /                                
-   //|   / /      ___       //|   / /        ___       ( )      __    
-  // |  / /     //   ) )   // |  / /       //   ) )   / /     //  ) ) 
- //  | / /     //   / /   //  | / /       //   / /   / /     //       
-//   |/ /     ((___( (   //   |/ /       ((___( (   / /     //        
+        /|    / /                /|    / /                                
+       //|   / /      ___       //|   / /        ___       ( )      __    
+      // |  / /     //   ) )   // |  / /       //   ) )   / /     //  ) ) 
+     //  | / /     //   / /   //  | / /       //   / /   / /     //       
+    //   |/ /     ((___( (   //   |/ /       ((___( (   / /     //        
 
 
                                 Please Login
@@ -38,6 +40,11 @@ class Main_menu:
 
                                 or [q] quit
 """
+        print(landing_ui)
+        """Returns the input from the user"""
+        input_str = input("login").lower()
+        return input_str
 
     def clear_screen(self):
+        """Clears the screen"""
         os.system("cls" if os.name == "nt" else "clear")
