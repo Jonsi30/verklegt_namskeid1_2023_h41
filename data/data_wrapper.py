6b6b4689c0cf0cc30ecac284c8709aff0efa5538
steinarorn <steinar98@gmail.com>
2023-12-05 13:21:24 +0000

    data_rapper

---
+++ data/data_wrapper.py
@@ -0,0 +1,9 @@
+from data.destination_data import Destination_data
+
+class Data_wrapper:
+    def __init__(self):
+        self.destination_data = Destination_data()
+    
+    def get_all_destinations(self):
+        return self.destination_data.get_all_destination()
+    
