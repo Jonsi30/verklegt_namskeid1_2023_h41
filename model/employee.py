d1985c66b4fd37b18ed7c240ca4fcb27ee367672
helgzen <143876664+helgzen@users.noreply.github.com>
2023-12-05 15:26:47 +0000

    update flight_list

---
+++ model/employee.py
@@ -0,0 +1,14 @@
+class Employee:
+    def __init__(self, name ="", role ="", rank ="", license ="", phone_nr ="", adress ="", ssn =""):
+        self.name = name
+        self.role = role
+        self.rank = rank
+        self.license = license
+        self.phone_nr = phone_nr
+        self.adress = adress
+        self.ssn = ssn
+
+    def __str__(self):
+        return f"name: {self.name}, role: {self.role}, rank: {self.rank}, license: {self.license}, phone nr: {self.phone_nr}, adress: {self.adress}, ssn: {self.ssn} "
+        
+
