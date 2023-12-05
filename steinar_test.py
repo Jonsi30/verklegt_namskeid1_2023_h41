66b382737805d260ccfce451aa4f7544e37b949f
steinarorn <steinar98@gmail.com>
2023-12-05 13:58:32 +0000

    data rapper

--- steinar_test.py
+++ steinar_test.py
@@ -1,10 +1,9 @@
-from data.destination_data import Destination_data
 from model.destination import Destination
-
+from data.data_wrapper import Data_wrapper
 def main():
     
-    name = Destination_data()
-    """
+    name = Data_wrapper()
+    
     id = str(input("Enter new id: "))
     id = id.upper()
     destination = input("Enter new destination: ")
@@ -12,9 +11,9 @@ def main():
 
     info = f'"{id}", "{destination}", "{numeric_id}"'
 
-    new_destination = Destination(f'{id}', f'{destination}', f'{numeric_id}')
-    name.create_destination(new_destination)"""
-    all_destinations = name.get_all_destination()
+    new_destination = (f'{id}', f'{destination}', f'{numeric_id}')
+    name.create_destination(new_destination)
+    all_destinations = name.get_all_destinations()
     
     for destination in all_destinations:
         
