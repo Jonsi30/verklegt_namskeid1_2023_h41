fdb3f07199dcafdc52e009534b4cd64795e47500
steinarorn <steinar98@gmail.com>
2023-12-05 13:07:50 +0000

    data til að lesa úr destinations

--- Data_layer/destination_data.py
+++ Data_layer/destination_data.py
@@ -3,9 +3,10 @@ from model.destination import Destination
 
 class Destination_data:
     def __init__(self):
-        self.file_name = "verklegt_namskeid1_2023_h41/files/destinations.csv" #filenameið sem inniheldur info um þetta class
+        self.file_name = "../verklegt_namskeid1_2023_h41/files/destinations.csv" #filenameið sem inniheldur info um þetta class
 
     def get_all_destination(self):
+        
         ret_list = []
         with open(self.file_name, newline='', encoding="utf-8") as csvfile:
             reader = csv.DictReader(csvfile)
@@ -13,13 +14,13 @@ class Destination_data:
                 ret_list.append(Destination(row["id"], row["destination"], row["numeric_id"]))
         return ret_list
 
-    def create_destination(self):
+    def create_destination(self, destination_instance):
         #User can Create Destination
         with open(self.file_name, 'a', newline='', encoding="utf-8") as csvfile:
             fieldnames = ["id", "destination", "numeric_id"]
             writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
 
-            writer.writerow({'id': Destination.id,'destination': Destination.destination, 'numeric_id': Destination.numeric_id})
+            writer.writerow({'id': destination_instance.id,'destination': destination_instance.destination, 'numeric_id': destination_instance.numeric_id})
 
 
 """
--- steinar_test.py
+++ steinar_test.py
@@ -1,8 +1,27 @@
 from Data_layer.destination_data import Destination_data
 from model.destination import Destination
 
+def main():
+    
+    name = Destination_data()
+    """
+    id = str(input("Enter new id: "))
+    id = id.upper()
+    destination = input("Enter new destination: ")
+    numeric_id = input("Enter numeric id: ")
 
-name = Destination_data
+    info = f'"{id}", "{destination}", "{numeric_id}"'
+
+    new_destination = Destination(f'{id}', f'{destination}', f'{numeric_id}')
+    name.create_destination(new_destination)"""
+    all_destinations = name.get_all_destination()
+    
+    for destination in all_destinations:
+        
+        print(destination)
+
+    
 
 
-print(name.get_all_destination)
+if __name__ == "__main__":
+    main()
