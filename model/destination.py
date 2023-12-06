8333be2be9356f2f128bae040af50cafe08ad510
SindriMar <sindri63@gmail.com>
2023-12-06 13:52:02 +0000

    commit my stuff

--- model/destination.py
+++ model/destination.py
@@ -1,8 +1,10 @@
 class Destination:
-    def __init__(self, id="",destination ="",numeric_id =""):
-        self.id = id
+    def __init__(self,destination ="",distance_from_ice ="",flight_time ="",emergency_contact ="",emergency_contact_phone_nr =""):
         self.destination = destination
-        self.numeric_id = numeric_id
+        self.distance_from_ice = distance_from_ice
+        self.flight_time = flight_time
+        self.emergency_contact = emergency_contact
+        self.emergency_contact_phone_nr = emergency_contact_phone_nr
 
     def __str__(self):
-        return f"id: {self.id}, destination: {self.destination}, numeric_id: {self.numeric_id} "
+        return f"destination: {self.destination}, distance from Iceland: {self.distance_from_ice}, flight time: {self.flight_time}, emergency contact: {self.emergency_contact},{self.emergency_contact_phone_nr}"
