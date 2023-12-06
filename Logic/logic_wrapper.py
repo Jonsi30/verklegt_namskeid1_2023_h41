78ea3275992618b6008a59a2355d3bb767e90ddc
Jón Ágústsson <jonsarinn30@gmail.com>
2023-12-06 11:20:09 +0000

    hehe

--- Logic/logic_wrapper.py
+++ Logic/logic_wrapper.py
@@ -1,12 +1,14 @@
 from logic.voyage_formatter import VoyageFormatter
 from logic.voyage_details import Voyage_details
 from Logic.logic_destination import Logic_destination
+from data.data_wrapper import Data_wrapper
 
 class Logic_wrapper:
     def __init__(self):
         self.destinations = Logic_destination()
+        self.data_wrapper = Data_wrapper()
+        self.voyage_logic = Voyagelogic()
         
-    
     def all_destinations(self):
         return self.destinations.all_dest()
 
@@ -17,3 +19,8 @@ class Logic_wrapper:
             voyage
         )  # pass the Voyage instance to VoyageFormatter
         return voyage_formatter.format_voyage()
+    def get_all_voyages(self):
+        return self.voyage_logic.get_all_voyages()
+     
+    def format_voyage(self):
+        return self.voyage_logic.format_voyage()
