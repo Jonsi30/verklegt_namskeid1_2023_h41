b7ad8486fa28a7bb203f68426486c27022855056
SindriMar <sindri63@gmail.com>
2023-12-06 15:24:21 +0000

    updated employeeLL

--- Logic/logic_employees.py
+++ Logic/logic_employees.py
@@ -14,10 +14,32 @@ class LogicEmployees:
         return self.employee.get_all_employees()
     
     def update_employee(self,role,rank,license,phone,address):
-        updated_info = role,rank,license,phone,address
+        updated_info = role, rank, license, phone, address
 
         return self.employee.update_employee(updated_info)
     
+    def get_all_pilots(self,name,role,license):
+        pilot_info = name, role, license
+
+        return self.employee.get_all_pilots(pilot_info)
+    
+    def get_all_crew(self,name,role,rank):
+        crew_info = name, role, rank
+
+        return self.employee.get_all_crew(crew_info)
+    
+    def get_occupied_staff():
+        pass
+
+    def get_available_staff():
+        pass
+    
+    def get_by_ssn():
+        pass
+
+    def check_if_available():
+        pass
+
     
 
         
