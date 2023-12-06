c7b389ec114d360da3a7d7ba56e4242c37165cf5
oppenheimm <tommibragi03@gmail.com>
2023-12-06 11:20:42 +0000

    asdf

--- Logic/voyage_logic.py
+++ Logic/voyage_logic.py
@@ -9,4 +9,29 @@ class VoyageLogic:
 
     def get_all_voyages(self):
         os.system("cls" if os.name == "nt" else "clear")
-        return self.file
+        flight_dict = dict()
+        lines = self.file.readlines()[1:]
+        for line in lines:
+            values = line.split(",")
+            if len(values) != 6:
+                continue
+            (
+                flight_nr,
+                dep_from,
+                arr_at,
+                departure,
+                arrival,
+                plane_insignia,
+            ) = values
+            dep_time, dep_date = departure.split(" ")
+            arr_time, arr_date = arrival.split(" ")
+            flight_dict[flight_nr] = {
+                "Departure From": dep_from,
+                "Arrival At": arr_at,
+                "Departure Time": dep_time,
+                "Departure Date": dep_date,
+                "Arrival Time": arr_time,
+                "Arrival Date": arr_date,
+                "Plane Insignia": plane_insignia,
+            }
+        return flight_dict
