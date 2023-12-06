8d4fc203a7aafd77d35bff92daae5457f022c0a6
steinarorn <steinar98@gmail.com>
2023-12-06 11:45:07 +0000

    logic_wrapper

--- Logic/logic_wrapper.py
+++ Logic/logic_wrapper.py
@@ -1,6 +1,6 @@
-from logic.logic_destination import Logic_destination
+from Logic.logic_destination import Logic_destination
 from data.data_wrapper import Data_wrapper
-from logic.voyage_logic import VoyageLogic
+from Logic.voyage_logic import VoyageLogic
 
 
 class Logic_wrapper:
@@ -9,8 +9,16 @@ class Logic_wrapper:
         self.data_wrapper = Data_wrapper()
         self.voyage_logic = VoyageLogic()
 
-    def all_destinations(self):
-        return self.destinations.all_dest()
+    def all_destinations(self):#Þetta gefur alla destinations listaða
+        #Logic.Logic wrapper -> Logic.Logic_destination -> data.Data_wrapper -> data.destination_data
+        #Hægt að keyra þetta fyrir utan þessa möppu með þessu formatti:
+        '''
+        from Logic.logic_wrapper import Logic_wrapper
 
+        dest = Logic_wrapper()
+
+        dest.all_destinations()
+        return self.destinations.all_dest()
+        '''
     def get_all_voyages(self):
         return self.voyage_logic.get_all_voyages()
