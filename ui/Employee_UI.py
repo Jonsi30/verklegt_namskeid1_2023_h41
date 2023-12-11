e6ff8cbabad6aed5a4f8e39ecd1702df9695a9b2
oppenheimm <tommibragi03@gmail.com>
2023-12-11 14:36:09 +0000

    shitt fokk

--- ui/Employee_UI.py
+++ ui/Employee_UI.py
@@ -16,7 +16,30 @@ class employee:
         return
 
     def get_all_crew(self):
-        return print(ascii().employee_list_art())
+        while True:
+            os.system("cls" if os.name == "nt" else "clear")
+            print(ascii().employee_list_art())
+            print("maggi faggi")
+            options = """
+        [P]ILOTS
+        [C]ABIN CREW
+        [Q]UIT
+        [B]ACK
+        """
+            print(options)
+            command = input("Enter yer command sire!: ")
+            command = command.lower()
+            if command == "q":
+                print("shutting down")
+                break
+            elif command == "b":
+                return
+            elif command == "0":
+                pass
+            elif command == "1":
+                pass
+            else:
+                print("invalid input!")
 
     def get_all_available_employees(self):
         return
