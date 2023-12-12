3176bb86ebf3ed61e1787f85361575bb66a726e7
SindriMar <sindri63@gmail.com>
2023-12-12 13:34:57 +0000

    comment

--- Logic/logic_wrapper.py
+++ Logic/logic_wrapper.py
@@ -14,6 +14,7 @@ class Logic_wrapper:
         self.logic_aircraft = LogicAircraft()
 
     def create_destination(self):
+        """Returns a new destination to the destination list"""
         return self.logic_destinations.create_destination()
 
     def get_all_destinations(self):
@@ -24,47 +25,59 @@ class Logic_wrapper:
         return self.logic_destinations.update_emergency_contact(updated_info)
 
     def create_employee(self, new_info):
+        """Returns a new employee to the employee list"""
         return self.logic_employees.create_employee(new_info)
 
     def get_all_employees(self):
+        """Returns all employess"""
         return self.logic_employees.get_all_employees()
-
-    def get_employee_dict(self, ret_dict):
+    
+    def get_employee_dict(self,ret_dict):
+        """Returns all employess in the employee dict"""
         return self.logic_employees.get_employee_dict(ret_dict)
-
+    
     def update_employee(self, updated_info):
+        """Returns an updated employee to the employee list"""
         return self.logic_employees.update_employee(updated_info)
 
     def get_all_pilots(self):
+        """Returns all pilots"""
         return self.logic_employees.get_all_pilots()
 
     def get_all_crew(self):
+        """Returns all crew members"""
         return self.logic_employees.get_all_crew()
 
     def update_destination(self, new_info):
+        """Returns an updated list of destinations"""
         return self.logic_destinations.update_destination_info(new_info)
-
+    
     def get_voyagelists_from_week(self, year_number, week_number):
         """Takes in a year, and a week for that given year, returns a list of all the voyages in a given week"""
         return self.voyage_logic.get_week_number(year_number, week_number)
 
     def get_occupied_staff(self):
+        """Returns a list of occupied staff"""
         return self.logic_employees.get_occupied_staff()
 
     def get_available_staff(self):
+        """Returns a list of available staff"""
         return self.logic_employees.get_available_staff()
 
-    def get_by_ssn(self):
-        return self.logic_employees.get_by_ssn()
+    def get_by_ssn(self, get_ssn):
+        """Returns the employ of given social security number"""
+        return self.logic_employees.get_by_ssn(get_ssn)
 
     def check_if_available(self):
         pass
 
-    def get_all_voyages(self):
-        return self.voyage_logic.display_voyage_manager()
-
+    # def get_all_voyages(self):
+    #     return self.voyage_logic.display_voyage_manager()
+    
     def display_voyage(self):
+        """Returns all voyages for the managers so the manager can put staff on voyage"""
         return self.voyage_logic.display_voyage_manager()
 
     def get_all_aircraft(self):
+        """Returns all aircrafts and the info about the aircrafts"""
         return self.logic_aircraft.get_all_aircrafts()
