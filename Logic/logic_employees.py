9c4552e2e6a0a1e0a9e8457985164abc760631aa
oppenheimm <tommibragi03@gmail.com>
2023-12-14 14:49:37 +0000

    mm

--- Logic/logic_employees.py
+++ Logic/logic_employees.py
@@ -2,6 +2,7 @@ from data.data_wrapper import Data_wrapper
 from model.employee import Employee
 from prettytable import PrettyTable
 
+
 class LogicEmployees:
     def __init__(self):
         self.employee = Data_wrapper()
@@ -22,14 +23,16 @@ class LogicEmployees:
     def get_all_pilots(self):
         """Returns a list with all the pilots working for the company NaN air."""
         pilot_list = []
-        
+
         all_employees = Data_wrapper().get_all_employees()
         for employee in all_employees:
             if employee.role == "Pilot":
-                pilot_list.append([employee.name, employee.role, employee.rank, employee.license])
+                pilot_list.append(
+                    [employee.name, employee.role, employee.rank, employee.license]
+                )
 
         return pilot_list
-        
+
     def get_all_cabin_crew(self):
         "Returns a list with all the cabin crew working for the company Nan Air"
         cabin_crew = []
@@ -37,7 +40,7 @@ class LogicEmployees:
         for employee in all_employees:
             if employee.role == "Cabincrew":
                 cabin_crew.append([employee.name, employee.rank])
-        
+
         return cabin_crew
 
     def get_all_crew(self):
@@ -51,7 +54,7 @@ class LogicEmployees:
 
     def get_employee_info(self, employee_name: str):
         """Returns a particlar employee and his attributes in a list. If the list is empty then no employee name matched the employess in the company"""
-        employee_list = [] 
+        employee_list = []
 
         for employee in Data_wrapper().get_all_employees():
             if employee_name == employee.name:
@@ -72,29 +75,27 @@ class LogicEmployees:
                 pass
         return occupied_list
 
-    def display_available_employees(self, date): 
+    def display_available_employees(self, date):
         """Takes in date in this format: '21:00 16.12.2023'"""
         """Returns a list of available employees for that particular date"""
         not_available_employees = []
-        
 
         for voyage in Data_wrapper().get_all_voyages():
             if date == voyage.arr_time_back[6:]:
-                if voyage.captain != 'Unassigned':
+                if voyage.captain != "Unassigned":
                     not_available_employees.append(voyage.captain)
-                if voyage.copilot != 'Unassigned':
+                if voyage.copilot != "Unassigned":
                     not_available_employees.append(voyage.copilot)
-                if voyage.head_of_service != 'Unassigned':
+                if voyage.head_of_service != "Unassigned":
                     not_available_employees.append(voyage.head_of_service)
-                if voyage.flight_attendant != 'Unassigned':
+                if voyage.flight_attendant != "Unassigned":
                     not_available_employees.append(voyage.flight_attendant)
         non_available = list(dict.fromkeys(not_available_employees).keys())
         available_employees = LogicEmployees().get_available_employees(non_available)
-       
-        return (available_employees)
+
+        return available_employees
 
     def get_available_employees(self, name_list):
-        
         availabe_employees = []
         all_employees = Data_wrapper().get_all_employees()
         for name in all_employees:
@@ -112,10 +113,8 @@ class LogicEmployees:
                 pass
 
         return employee_ssn
-    
 
     def get_schedule_by_ssn(self, ssn):
-        
         all_voyages = Data_wrapper().get_all_voyages()
         all_employees = Data_wrapper().get_all_employees()
         name = ""
@@ -124,24 +123,42 @@ class LogicEmployees:
             if employee.ssn == ssn:
                 name += employee.name
                 role += employee.role
-            
 
         voyages = []
 
         for voyage in all_voyages:
-            if name in voyage.captain or name in voyage.copilot or name in voyage.head_of_service or name in voyage.flight_attendant:
+            if (
+                name in voyage.captain
+                or name in voyage.copilot
+                or name in voyage.head_of_service
+                or name in voyage.flight_attendant
+            ):
                 voyages.append(voyage)
 
-        fieldnames = ["Voyage Id", "Employee", "Role", "Departure Time", "Arrival Time Back", "Destination"]
+        fieldnames = [
+            "Voyage Id",
+            "Employee",
+            "Role",
+            "Departure Time",
+            "Arrival Time Back",
+            "Destination",
+        ]
 
         if voyages:
             work_scheadule = PrettyTable()
             work_scheadule.field_names = fieldnames
             for work in voyages:
+                work_scheadule.add_row(
+                    [
+                        work.id,
+                        name,
+                        role,
+                        work.dep_time,
+                        work.arr_time_back,
+                        work.arr_at,
+                    ]
+                )
 
-                work_scheadule.add_row([work.id, name, role, work.dep_time, work.arr_time_back, work.arr_at])
+            return work_scheadule
 
-            return (work_scheadule)
-        
         return f"{name} doesn't have any upcoming voayges!"
-    
